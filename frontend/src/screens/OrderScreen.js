5f6357d54ed7c771cdae5389676341e21c92d9c9
Ahmad Magdy <amagsid@gmail.com>
2021-05-06 13:01:35 +0200

    finished admin screens part 2

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -2,24 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { PayPalButton } from 'react-paypal-button-v2';
 import { Link } from 'react-router-dom';
-import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import { Row, Col, ListGroup, Image, Card, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import {
   getOrderDetails,
   payOrder,
-  //   deliverOrder,
+  deliverOrder,
 } from '../actions/orderActions';
 import {
   ORDER_PAY_RESET,
-  // ORDER_DELIVER_RESET,
+  ORDER_DELIVER_RESET,
 } from '../constants/orderConstants';
 
-const OrderScreen = ({
-  match,
-  // history
-}) => {
+const OrderScreen = ({ match, history }) => {
   const orderId = match.params.id;
 
   const [sdkReady, setSdkReady] = useState(false);
@@ -32,11 +29,11 @@ const OrderScreen = ({
   const orderPay = useSelector((state) => state.orderPay);
   const { loading: loadingPay, success: successPay } = orderPay;
 
-  //   const orderDeliver = useSelector((state) => state.orderDeliver);
-  //   const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
+  const orderDeliver = useSelector((state) => state.orderDeliver);
+  const { loading: loadingDeliver, success: successDeliver } = orderDeliver;
 
-  //   const userLogin = useSelector((state) => state.userLogin);
-  //   const { userInfo } = userLogin;
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
 
   if (!loading) {
     // Calculate prices
@@ -50,9 +47,9 @@ const OrderScreen = ({
   }
 
   useEffect(() => {
-    // if (!userInfo) {
-    //   history.push('/login');
-    // }
+    if (!userInfo) {
+      history.push('/login');
+    }
 
     const addPayPalScript = async () => {
       const { data: clientId } = await axios.get('/api/config/paypal');
@@ -69,11 +66,12 @@ const OrderScreen = ({
 
     if (
       !order ||
-      successPay
-      // || successDeliver || order._id !== orderId
+      successPay ||
+      successDeliver
+      // || order._id !== orderId
     ) {
       dispatch({ type: ORDER_PAY_RESET });
-      //   dispatch({ type: ORDER_DELIVER_RESET });
+      dispatch({ type: ORDER_DELIVER_RESET });
       dispatch(getOrderDetails(orderId));
     } else if (!order.isPaid) {
       if (!window.paypal) {
@@ -82,13 +80,7 @@ const OrderScreen = ({
         setSdkReady(true);
       }
     }
-  }, [
-    dispatch,
-    order,
-    orderId,
-    successPay,
-    // successDeliver
-  ]);
+  }, [dispatch, order, orderId, successPay, successDeliver]);
 
   const successPaymentHandler = (paymentResult) => {
     console.log(paymentResult);
@@ -96,9 +88,9 @@ const OrderScreen = ({
     dispatch(payOrder(orderId, paymentResult));
   };
 
-  //   const deliverHandler = () => {
-  //     dispatch(deliverOrder(order));
-  //   };
+  const deliverHandler = () => {
+    dispatch(deliverOrder(order));
+  };
 
   return loading ? (
     <Loader />
@@ -225,7 +217,7 @@ const OrderScreen = ({
                 </ListGroup.Item>
               )}
 
-              {/*  {loadingDeliver && <Loader />}
+              {loadingDeliver && <Loader />}
               {userInfo &&
                 userInfo.isAdmin &&
                 order.isPaid &&
@@ -239,7 +231,7 @@ const OrderScreen = ({
                       Mark As Delivered
                     </Button>
                   </ListGroup.Item>
-                )} */}
+                )}
             </ListGroup>
           </Card>
         </Col>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,9 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import {
   productListReducer,
   productDetailsReducer,
+  productDeleteReducer,
+  productCreateReducer,
+  productUpdateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -19,14 +22,17 @@ import {
   orderCreateReducer,
   orderDetailsReducer,
   orderPayReducer,
-  // orderDeliverReducer,
+  orderDeliverReducer,
   orderListMyReducer,
-  // orderListReducer,
+  orderListReducer,
 } from './reducers/orderReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  productDelete: productDeleteReducer,
+  productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
@@ -38,7 +44,9 @@ const reducer = combineReducers({
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
+  orderDeliver: orderDeliverReducer,
   orderListMy: orderListMyReducer,
+  orderList: orderListReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
